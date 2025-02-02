1f148f64a58bd453e9966db0688ddc27bd4dbb99
Jordan Higgins <jordanhiggins@usf.edu>
2025-02-01 20:17:33 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+// Task 1: Customer Discounts
+let purchaseAmount = 999;
+if (purchaseAmount > 100) {
+    discountAmount = purchaseAmount * 0.4;
+    console.log("40% New Customer Discount!")
+}
+
+console.log(`Discounted Total: $${discountAmount}`);
+// Declared the purchase amount, the discounted amount, and have a discount message all within the console.
