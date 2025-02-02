7a7f7c608ed826fababb04e7dd09cc0f5f5dec00
Jordan Higgins <jordanhiggins@usf.edu>
2025-02-01 20:34:09 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -7,3 +7,13 @@ if (purchaseAmount > 100) {
 
 console.log(`Discounted Total: $${discountAmount}`);
 // Declared the purchase amount, the discounted amount, and have a discount message all within the console.
+
+// Task 2: Sales Report
+let sales = [200, 300, 150, 400, 750]
+let totalSales = 0;
+for ( let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+
+console.log (`Total Sales: $${totalSales}`);
+// Declared the array "sales" and assigned values to indicate the sales. Then created a for loop calculating total sales and printing it to the console.
